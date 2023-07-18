2cc09bdbd76c3e15d7b8dc307aa9e167294f51e4
David Gamero <david340804@gmail.com>
2023-07-17 21:42:59 -0400

    i am the router now

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
-// use std::collections::HashMap;
+use std::collections::HashMap;
 
-// use serde::{Serialize, Deserialize};
-// use serde_json::Value;
-// use anyhow::Result;
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 // use spin_sdk::{
 //     http_component,
 //     http::{
-//         Request, 
-//         Response, 
-//         Router, 
+//         Request,
+//         Response,
+//         Router,
 //         Params,
 //     },
 // };
-use anyhow::Result;
 use spin_sdk::{
     http::{Request, Response},
     http_component,
@@ -22,100 +21,147 @@ use spin_sdk::{
 #[http_component]
 fn handle_wasmambo5(req: Request) -> Result<Response> {
     println!("{:?}", req.headers());
+
+    let mut router = ImTheRouterNow::new();
+    router.add_route("/move", api::move_);
+    router.add_route("/", api::info);
+    router.add_route("/somethingelse", api::echo_wildcard);
+
+    let routed = router.handle(req);
+    match routed {
+        Ok(r) => {
+            println!("routed");
+            return Ok(r);
+        }
+        Err(e) => {
+            println!("error");
+        }
+    }
+
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
         .body(Some("Hello, Fermyon".into()))?)
 }
 
-// mod logic;
+// uSe WeB AsSeMbLy iTs tHe FuTuRe hErE I Am mAkiNg a RoUtEr iN 2023
+pub struct ImTheRouterNow {
+    routes: Vec<(&'static str, fn(Request) -> Result<Response>)>,
+}
 
-// /// A Spin HTTP component that internally routes requests.
-// #[http_component]
-// fn handle_route(req: Request) -> Result<Response> {
-//     let mut router = Router::new();
-//     router.get("/", api::info);
-//     router.post("/move", api::move_);
-//     router.any("/*", api::echo_wildcard);
-//     router.handle(req)
-// }
+impl ImTheRouterNow {
+    pub fn new() -> Self {
+        Self { routes: Vec::new() }
+    }
 
-// mod api {
-//     use super::*;
+    pub fn add_route(&mut self, path: &'static str, handler: fn(Request) -> Result<Response>) {
+        self.routes.push((path, handler));
+    }
 
-//     // /hello/:planet
-//     pub fn info(_req: Request, params: Params) -> Result<Response> {
-//         //let planet = params.get("planet").expect("PLANET");
+    pub fn handle(&self, req: Request) -> Result<Response> {
+        let u = req.uri().to_string();
+        println!("uri: {}", u);
+        for (path, handler) in &self.routes {
+            if u.starts_with(path) {
+                return handler(req);
+            }
+        }
+        Ok(http::Response::builder()
+            .status(http::StatusCode::NOT_FOUND)
+            .body(None)?)
+    }
+}
 
-//         Ok(http::Response::builder()
-//             .status(http::StatusCode::OK)
-//             .body(Some(logic::info()))?)
-//     }
-    
-//     pub fn move_(_req: Request, params: Params) -> Result<Response> {
+fn info(_req: Request) -> Result<Response> {
+    //let planet = params.get("planet").expect("PLANET");
 
-//         let b = _req.body().as_ref().unwrap();
-//         //turn b into GAmestate
-//         let gs: GameState = serde_json::from_slice(&b).unwrap();
-        
-//         Ok(http::Response::builder()
-//            .status(http::StatusCode::OK)
-//            .body(Some(
-//                    logic::get_move(
-//                        &gs.game,
-//                        &gs.turn,
-//                        &gs.board,
-//                        &gs.you          
-//                        )))?)
-//     }
+    Ok(http::Response::builder()
+        .status(http::StatusCode::OK)
+        .body(Some(logic::info()))?)
+}
 
-//     // /*
-//     pub fn echo_wildcard(_req: Request, params: Params) -> Result<Response> {
-//         let capture = params.wildcard().unwrap_or_default();
-//         Ok(http::Response::builder()
-//             .status(http::StatusCode::OK)
-//             .body(Some(format!("{capture}").into()))?)
-//     }
-// }
+mod logic;
 
-// #[derive(Serialize,Deserialize)]
-// pub struct Game {
-//     id: String,
-//     ruleset: HashMap<String, Value>,
-//     timeout: u32,
-// }
+mod api {
+    use super::*;
 
-// #[derive(Serialize,Deserialize)]
-// pub struct Board {
-//     height: u32,
-//     width: u32,
-//     food: Vec<Coord>,
-//     snakes: Vec<Battlesnake>,
-//     hazards: Vec<Coord>,
-// }
+    // /hello/:planet
+    pub fn info(_req: Request) -> Result<Response> {
+        //let planet = params.get("planet").expect("PLANET");
 
-// #[derive(Serialize,Deserialize)]
-// pub struct Battlesnake {
-//     id: String,
-//     name: String,
-//     health: u32,
-//     body: Vec<Coord>,
-//     head: Coord,
-//     length: u32,
-//     latency: String,
-//     shout: Option<String>,
-// }
+        Ok(http::Response::builder()
+            .status(http::StatusCode::OK)
+            .body(Some(logic::info()))?)
+    }
 
-// #[derive(Serialize,Deserialize)]
-// pub struct Coord {
-//     x: u32,
-//     y: u32,
-// }
+    pub fn move_(_req: Request) -> Result<Response> {
+        let b = _req.body().as_ref().unwrap();
+        //turn b into GAmestate
+        let r: std::result::Result<GameState, serde_json::Error> = serde_json::from_slice(&b);
+        match r {
+            Ok(gs) => Ok(http::Response::builder()
+                .status(http::StatusCode::OK)
+                .body(Some(logic::get_move(
+                    &gs.game, &gs.turn, &gs.board, &gs.you,
+                )))?),
+            Err(e) => {
+                println!("error: {:?}", e);
+                Ok(http::Response::builder()
+                    .status(http::StatusCode::BAD_REQUEST)
+                    .body(Some("error".into()))?)
+            }
+        }
+    }
 
-// #[derive(Serialize,Deserialize)]
-// pub struct GameState {
-//     game: Game,
-//     turn: u32,
-//     board: Board,
-//     you: Battlesnake,
-// }
+    // /*
+    pub fn echo_wildcard(_req: Request) -> Result<Response> {
+        // let capture = params.wildcard().unwrap_or_default();
+        Ok(http::Response::builder()
+            .status(http::StatusCode::OK)
+            .body(Some(
+                format!("my patience is exhaused. go make this method yourself.").into(),
+            ))?)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Game {
+    id: String,
+    ruleset: HashMap<String, Value>,
+    timeout: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Board {
+    height: u32,
+    width: u32,
+    food: Vec<Coord>,
+    snakes: Vec<Battlesnake>,
+    hazards: Vec<Coord>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Battlesnake {
+    id: String,
+    name: String,
+    health: u32,
+    body: Vec<Coord>,
+    head: Coord,
+    length: u32,
+    latency: String,
+    shout: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Coord {
+    x: u32,
+    y: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GameState {
+    game: Game,
+    turn: u32,
+    board: Board,
+    you: Battlesnake,
+}
